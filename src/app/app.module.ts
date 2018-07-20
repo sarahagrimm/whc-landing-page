56364e4018df07fc81dbdfab95327b301e07c0d8
Sarah Grimm <sarahagrimm@gmail.com>
2018-07-19 20:29:42 -0700

    added section that contained a get started button and background image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { AppstoreComponent } from './appstore/appstore.component';
 import { FeaturesComponent } from './features/features.component';
+import { GetStartedComponent } from './get-started/get-started.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { FeaturesComponent } from './features/features.component';
     NavbarComponent,
     HeaderComponent,
     AppstoreComponent,
-    FeaturesComponent
+    FeaturesComponent,
+    GetStartedComponent
   ],
   imports: [
     BrowserModule
